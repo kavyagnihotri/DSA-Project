44c17db501660207b458e4a28f4de4dfe6854638
kavyagnihotri <48334805+kavyagnihotri@users.noreply.github.com>
2022-02-16 19:32:08 +0530

    Add struct part -k

--- main.cpp
+++ main.cpp
@@ -2,11 +2,56 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct XY
-{
+// Simple coordinate object to represent points and vectors
+struct XY {
+    float x;
+    float y;
 
+    void pConstruct(float x, float y) {
+        x = x;
+        y = y;
+    }
 };
 
+// Axis-aligned bounding box with half dimension and center
+struct AABB {
+    XY center;
+    float halfDimension;
+
+    void construct(XY center, float halfDimension) {
+        center = center;
+        halfDimension = halfDimension;
+    }
+    bool containsPoint(XY point) {
+        // corners
+        float x1 = center.x - halfDimension;
+        float y1 = center.y - halfDimension;
+
+        float x2 = center.x - halfDimension;
+        float y2 = center.y + halfDimension;
+        
+        float x3 = center.x + halfDimension;
+        float y3 = center.y + halfDimension;
+
+        float x4 = center.x + halfDimension;
+        float y4 = center.y - halfDimension;
+
+        float x0 = point.x;
+        float y0 = point.y;
+
+        if(x0 > x1 && y0 > y1 && x0 > x2 && y0 < y2 && x0 < x2 && y0 < y2 && x0 < x4 && y0 > y4) 
+            return true;
+        else return false;
+    }
+
+    // boundary not included
+    bool intersectsAABB(AABB other) {
+        if(abs(center.x - other.center.x) < halfDimension + other.halfDimension 
+            && abs(center.y - other.center.y) < halfDimension + other.halfDimension) 
+            return true;
+        else return false;
+    }
+};
 
 bool QuadTrees::insert(XY p)
 {
